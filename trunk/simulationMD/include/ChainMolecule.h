1cc947a14331e84f752076c6246a2b16867645ac
alfaceor <alfaceor@d2e8890e-6934-46bd-f81c-84343ebeac70>
2012-01-01 21:04:05 +0000

    simulation particle chain

---
+++ trunk/simulationMD/include/ChainMolecule.h
@@ -0,0 +1,21 @@
+//
+//
+//
+//  @ Project : Untitled
+//  @ File Name : ChainMolecule.h
+//  @ Date : 12/4/2011
+//  @ Author : 
+//
+//
+
+
+#if !defined(_CHAINMOLECULE_H)
+#define _CHAINMOLECULE_H
+
+
+class ChainMolecule {
+public:
+	int arrayParticle: Particle[5];
+};
+
+#endif  //_CHAINMOLECULE_H
