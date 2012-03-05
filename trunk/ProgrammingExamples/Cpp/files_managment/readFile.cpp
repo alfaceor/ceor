a935f075fcc2ab237f1643076226de3d28d4a721
alfaceor <alfaceor@d2e8890e-6934-46bd-f81c-84343ebeac70>
2012-03-05 18:21:22 +0000

    now readfile works fine read a double data from a file and then put them i an 2d array

--- trunk/ProgrammingExamples/Cpp/files_managment/readFile.cpp
+++ trunk/ProgrammingExamples/Cpp/files_managment/readFile.cpp
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_array2d(double *array,const int N,const int M){
+	for (int i=0;i<N;i++){
+		for (int j=0;j<M;j++){
+			printf("%f\t",*(array+i*M+j));
+		}
+		printf("\n");
+	}
+}
+
+
 void print_array(double *array,const int N){
 	for (int i=0;i<N;i++){
 		printf("%f\n",*(array+i));
@@ -15,15 +25,23 @@ int main(int argc, char* argv[]) {
 
 	double number;
 	double arraynum[6];
+	double arraynum2d[6][2];
+	for (int i=0;i<6;i++){
+		for(int j=0;j<2;j++){
+			arraynum2d[i][j]=10.0*i+j;
+		}
+	}
+
 	printf("read file\n");
 	
 	for (int i=0;i<6;i++){
+		for (int j=0;j<2;j++)
 		// fscanf(fp,"%lf",&number);
-		fscanf(fp,"%lf",&arraynum[i]);
+			fscanf(fp,"%lf",&arraynum2d[i][j]);
 //		printf("number = %f\n",arraynum[i]);
 	}
 	
-	print_array(arraynum,6);
+	print_array2d(arraynum2d[0],6,2);
 	fclose(fp);
 	return EXIT_SUCCESS;
 }
