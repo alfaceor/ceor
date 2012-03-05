b58ed40ae44b85ad7aa6925c4fa687fab8f470ee
alfaceor <alfaceor@d2e8890e-6934-46bd-f81c-84343ebeac70>
2012-03-05 18:10:08 +0000

    readfile now reads a double and put the data in an array next step two columm array

--- trunk/ProgrammingExamples/Cpp/files_managment/readFile.cpp
+++ trunk/ProgrammingExamples/Cpp/files_managment/readFile.cpp
@@ -2,16 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_array(double *array,const int N){
+	for (int i=0;i<N;i++){
+		printf("%f\n",*(array+i));
+	}
+}
+
 
 int main(int argc, char* argv[]) {
 	FILE *fp;
 	fp = fopen("data.dat","r");
+
 	double number;
+	double arraynum[6];
 	printf("read file\n");
+	
 	for (int i=0;i<6;i++){
-		fscanf(fp,"%lf",&number);
-		printf("number = %f\n",number);
+		// fscanf(fp,"%lf",&number);
+		fscanf(fp,"%lf",&arraynum[i]);
+//		printf("number = %f\n",arraynum[i]);
 	}
+	
+	print_array(arraynum,6);
 	fclose(fp);
 	return EXIT_SUCCESS;
 }
