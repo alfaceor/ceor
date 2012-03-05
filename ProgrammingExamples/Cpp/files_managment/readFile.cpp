65c4ff668713d4d25f54566c63222c40af921ba5
alfaceor <alfaceor@d2e8890e-6934-46bd-f81c-84343ebeac70>
2012-03-05 18:01:44 +0000

    readFile with double just need to especify %lf (long float)

--- ProgrammingExamples/Cpp/files_managment/readFile.cpp
+++ ProgrammingExamples/Cpp/files_managment/readFile.cpp
@@ -6,11 +6,11 @@
 int main(int argc, char* argv[]) {
 	FILE *fp;
 	fp = fopen("data.dat","r");
-	int number;
+	double number;
 	printf("read file\n");
 	for (int i=0;i<6;i++){
-		fscanf(fp,"%d",&number);
-		printf("number = %d\n",number);
+		fscanf(fp,"%lf",&number);
+		printf("number = %f\n",number);
 	}
 	fclose(fp);
 	return EXIT_SUCCESS;
