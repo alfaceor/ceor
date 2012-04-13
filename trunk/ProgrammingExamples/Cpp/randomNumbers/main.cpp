9e6c55621f91913f3811b410020b1d3e2d24a3e8
alfaceor <alfaceor@d2e8890e-6934-46bd-f81c-84343ebeac70>
2012-04-13 13:29:14 +0000

    contourplot, histogram example

--- trunk/ProgrammingExamples/Cpp/randomNumbers/main.cpp
+++ trunk/ProgrammingExamples/Cpp/randomNumbers/main.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_rng.h>
+
 
 int main(int argc, char* argv[]) {
-	printf("Hello World!!\n");
+	gsl_rng *r1;
+	
+	const gsl_rng_type * T;
+	
+	T		= gsl_rng_default;
+	r1	= gsl_rng_alloc(T);
+	
+	double randomaux;
+	int N=atoi(argv[1]);
+	double sigma=atof(argv[2]);
+	for (int i=0; i<N; i++){
+		randomaux	= gsl_ran_gaussian(r1,sigma);
+		printf("%f\n",randomaux);
+	}
+	
 }
