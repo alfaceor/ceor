771403fd0f5b3643f2394639fd559e548d469fe7
alfaceor <alfaceor@d2e8890e-6934-46bd-f81c-84343ebeac70>
2012-04-17 13:15:20 +0000

    contour plot for energy landscape

--- ProgrammingExamples/python/moreSimple.py
+++ ProgrammingExamples/python/moreSimple.py
@@ -4,14 +4,14 @@ Created on Sun Apr 15 12:53:32 2012
 
 @author: alfaceor
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 x,y,z = np.loadtxt("datatest.dat",usecols=(5,4,3)).T
-xbinsize=0.5
-ybinsize=0.1
-xi = np.arange(x.min(),x.max()+xbinsize,xbinsize)
-yi = np.arange(y.min(),y.max()+ybinsize,ybinsize)
+#xbinsize=0.5
+#ybinsize=0.1
+# is better use linspace instead arange for floating points.
+xi, xbinsize = np.linspace(x.min(),x.max(),num=20,retstep=True)
+yi, ybinsize = np.linspace(y.min(),y.max(),num=20,retstep=True)
 
 xim,yim = np.meshgrid(xi,yi)
 
@@ -30,7 +30,6 @@ for row in range(nrow):
     for col in range(ncol):
         # TODO: se tiene que testar si existe un
         grid[row, col]=np.nan
-
         xc = xim[row, col]    # x coordinate.
         yc = yim[row, col]    # y coordinate.
 
@@ -41,6 +40,7 @@ for row in range(nrow):
         ind  = np.where(ibin == True)[0]
         # Allocate the values in corresponding grids
         zsum=0
+        print len(ind)
         if len(ind) != 0:
             for val in ind:
                 zsum=zsum+z[val]
@@ -49,9 +49,9 @@ for row in range(nrow):
             grid[row,col] = np.nan
 
 # ahora tengo q colocar los valores adecuados
-extent = (x.min(), x.max(), y.min(), y.max()) # extent of the plot
+extent = (xi.min(), xi.max(), yi.min(), yi.max()) # extent of the plot
 #plt.contourf(xim,yim,grid)
-plt.imshow(grid, extent=extent, cmap=None, origin='lower', vmin=z.min(), vmax=z.max(), aspect='auto', interpolation='bilinear')
+plt.imshow(grid, extent=extent, cmap=None, origin='lower', vmin=-5, vmax=z.max(), aspect='auto', interpolation='nearest')
 plt.colorbar()
 plt.scatter(x,y,marker='o',c='b',s=1)
 plt.show()
