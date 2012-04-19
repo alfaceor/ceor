5fd4984198d1aabde91ea059e032e55071d449e2
alfaceor <alfaceor@d2e8890e-6934-46bd-f81c-84343ebeac70>
2012-04-19 04:35:21 +0000

    En este ejemplo se demuestra que siempre dan los mismos numeros aleatorios

--- trunk/ProgrammingExamples/Cpp/randomNumbers/main.cpp
+++ trunk/ProgrammingExamples/Cpp/randomNumbers/main.cpp
@@ -6,19 +6,22 @@
 
 
 int main(int argc, char* argv[]) {
-	gsl_rng *r1;
+	gsl_rng *r1, *r2;
 	
-	const gsl_rng_type * T;
+	const gsl_rng_type * T1, * T2;
 	
-	T		= gsl_rng_default;
-	r1	= gsl_rng_alloc(T);
+	T1	= gsl_rng_default;
+	T2	= gsl_rng_default;
+	r1	= gsl_rng_alloc(T1);
+	r2	= gsl_rng_alloc(T2);
 	
-	double randomaux;
+	double randomaux1,randomaux2;
 	int N=atoi(argv[1]);
 	double sigma=atof(argv[2]);
 	for (int i=0; i<N; i++){
-		randomaux	= gsl_ran_gaussian(r1,sigma);
-		printf("%f\n",randomaux);
+		randomaux1	= gsl_ran_gaussian(r1,sigma);
+		randomaux2	= gsl_ran_gaussian(r2,sigma);
+		printf("%f\t%f\n",randomaux1,randomaux2);
 	}
 	
 }
