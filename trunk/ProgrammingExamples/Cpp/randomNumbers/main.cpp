1b5ef6eda44e955121d7dc00ad31979a0e1ce0d2
alfaceor <alfaceor@d2e8890e-6934-46bd-f81c-84343ebeac70>
2012-04-26 06:13:41 +0000

    experiments with the random numbers using gsl library

--- trunk/ProgrammingExamples/Cpp/randomNumbers/main.cpp
+++ trunk/ProgrammingExamples/Cpp/randomNumbers/main.cpp
@@ -4,10 +4,9 @@
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_rng.h>
 
+void generate2randomNumbers(int N, double sigma){
 
-int main(int argc, char* argv[]) {
 	gsl_rng *r1, *r2;
-	
 	const gsl_rng_type * T1, * T2;
 	
 	T1	= gsl_rng_default;
@@ -16,12 +15,34 @@ int main(int argc, char* argv[]) {
 	r2	= gsl_rng_alloc(T2);
 	
 	double randomaux1,randomaux2;
-	int N=atoi(argv[1]);
-	double sigma=atof(argv[2]);
 	for (int i=0; i<N; i++){
 		randomaux1	= gsl_ran_gaussian(r1,sigma);
 		randomaux2	= gsl_ran_gaussian(r2,sigma);
 		printf("%f\t%f\n",randomaux1,randomaux2);
 	}
 	
+}
+
+void generateRandomNumber(gsl_rng *r, double sigma){
+	double randomaux;
+	randomaux	= gsl_ran_gaussian(r,sigma);
+	printf("%f\n",randomaux);
+}
+
+int main(int argc, char* argv[]) {
+	int N=atoi(argv[1]);
+	double sigma=atof(argv[2]);
+//	generate2randomNumbers(N,sigma);
+//	generate2randomNumbers(N,sigma);
+
+	const gsl_rng_type * T1;
+	gsl_rng *r1;
+	
+	T1	= gsl_rng_default;
+	r1	= gsl_rng_alloc(T1);
+
+	for (int i=0; i<N; i++){
+		generateRandomNumber(r1,sigma);
+	}
+
 }
